65e22fb82725eeee9cecee290d931022ba6a4a09
Michał Sitko <pierwszy1@gmail.com>
2014-09-11 17:57:13 +0200

    bug causing multiple tests evaluation when more than one spec files present

--- src/main/resources/sbtjasmine.js
+++ src/main/resources/sbtjasmine.js
@@ -173,8 +173,11 @@ function runTests(appJsRoot, appJsLibRoot, testRoot, confFile, envHtml) {
         });
     }
 
-    for (i = 0, len = EnvJasmine.specs.length >>> 0; i < len; i += 1) {
+    for (i = 0, len = EnvJasmine.specs.length; i < len; ++i) {
         try {
+            // hack required to avoid evaluating previous tests again
+            jasmine.getEnv().topSuite().children = [];
+
             EnvJasmine.currentScope = {};
             EnvJasmine.load = EnvJasmine.loadFactory(EnvJasmine.currentScope);
             EnvJasmine.specFile = EnvJasmine.specs[i];
@@ -188,6 +191,7 @@ function runTests(appJsRoot, appJsLibRoot, testRoot, confFile, envHtml) {
             }
             var specLoader = 'require(["' + specPathOsSpecific + '"]);';
             EnvJasmine.cx.evaluateString(EnvJasmine.currentScope, specLoader, 'Loading '+EnvJasmine.specFile, 0, null);
+
             print("running the jasmine tests");
             var envHtmlOsSpecific = '' + envHtml; // make sure it's a javascript string object rather than a java object
             if(EnvJasmine.environment == 'WIN') {
@@ -195,6 +199,7 @@ function runTests(appJsRoot, appJsLibRoot, testRoot, confFile, envHtml) {
             }
             var windowLoader = 'window.location.assign("file://' + envHtmlOsSpecific + '")';
             EnvJasmine.cx.evaluateString(EnvJasmine.currentScope, windowLoader, 'Executing '+EnvJasmine.specs[i], 0, null);
+            print();
         } catch (e) {
 
              print(EnvJasmine.red('error running jasmine test: ' + EnvJasmine.specs[i] + "\n error was: " + e));
