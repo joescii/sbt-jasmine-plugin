abb0b059d3d9e375dfbda5463c70944b0a1ce03d
Joe Barnes <barnesjd@gmail.com>
2015-10-11 11:03:38 -0500

    After testing, I discovered that boot-rhino.js is not shipped with the jasmine2 webjars

--- src/main/resources/jasmine2Env.js
+++ src/main/resources/jasmine2Env.js
@@ -4,5 +4,5 @@
 EnvJasmine.loadLibGlobal("rjs/r-2.0.1.js");
 EnvJasmine.loadLibGlobal("envjs/env.rhino.1.2.js");
 EnvJasmine.loadLibGlobal(jasmineResourceRoot+"/jasmine.js");
-EnvJasmine.loadLibGlobal(jasmineResourceRoot+"/boot-rhino.js");
+EnvJasmine.loadLibGlobal("jasmine2/boot-rhino.js");
 EnvJasmine.loadLibGlobal("jasmine2-rhino-reporter/jasmine-rhino-reporter.js");
